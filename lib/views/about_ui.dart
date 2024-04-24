b6ad3d8b39defeaa7aaa3e92fe300c537471fed8
TS2811 <S6419410003@sau.ac.th>
2024-04-25 02:17:37 +0700

    Code V.5 : All Complete

--- lib/views/about_ui.dart
+++ lib/views/about_ui.dart
@@ -14,6 +14,72 @@ class _AboutUIState extends State<AboutUI> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.width * 0.5,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle, // กำหนดรูปร่างเป็นวงกลม
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/saulogo.png',
+                  ), // รูปภาพของวงกลม
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              'Tech SAU BUFFET',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.03,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 252, 94, 94),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Text(
+              'แอปพลิเคชั้นร้านหมูกะทะ\nเพื่อคนไทย\nโดยเด็กไทย\nสนใจแอปพลิเคชั้นติดต่อ',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'เด็กไอที SAU',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.035,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.width * 0.4,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/sauqr.png',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
